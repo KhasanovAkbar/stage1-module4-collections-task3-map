a19d8b4e385ec0121a9a9532126e706ff504a6f6
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-07-25 16:46:45 +0500

    first commit

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+
+        Map<String, Integer> result = new HashMap<>();
+        if (sourceMap.size() > 0) {
+            for (Map.Entry<Integer, String> item : sourceMap.entrySet()) {
+                if (result.containsKey(item.getValue())) {
+                    if (result.get(item.getValue()) > item.getKey())
+                        result.put(item.getValue(), item.getKey());
+                } else
+                    result.put(item.getValue(), item.getKey());
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,34 @@
 package com.epam.mjc.collections.map;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> result = new HashMap<>();
+
+        if (!sentence.trim().equals("")) {
+            String[] words = sentence.toLowerCase().trim().split(" ");
+            List<String> inputWords = new ArrayList<>();
+            for (String item:words){
+                if (item.contains(".") || item.contains(",")){
+                    inputWords.add(item.substring(0, item.length()-1));
+                } else
+                    inputWords.add(item);
+            }
+            for (String item : inputWords) {
+                if (!item.equals("")) {
+                    if (result.containsKey(item)) {
+                        Integer value = result.get(item);
+                        result.put(item, ++value);
+                    } else
+                        result.put(item, 1);
+                }
+            }
+        }
+        return result;
+
     }
 }
